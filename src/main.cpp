15f266021e3d36524a1bd58cbe66767ce35b94f4
Alistair Symonds <alistair.symonds@gmail.com>
2020-09-01 00:52:13 +1000

    Updated main to perform one rotation

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,10 @@
                       // Watterott TMC5160 uses 0.075
 
 // Select your stepper driver type
-//TMC2130Stepper driver(CS_PIN, R_SENSE);                           // Hardware SPI
-//TMC2130Stepper driver(CS_PIN, R_SENSE, SW_MOSI, SW_MISO, SW_SCK); // Software SPI
-//TMC2660Stepper driver(CS_PIN, R_SENSE);                           // Hardware SPI
-//TMC2660Stepper driver(CS_PIN, R_SENSE, SW_MOSI, SW_MISO, SW_SCK);
-//TMC5160Stepper driver(CS_PIN, R_SENSE);
-//TMC5160Stepper driver(CS_PIN, R_SENSE, SW_MOSI, SW_MISO, SW_SCK);
 
-//TMC2208Stepper driver(&SERIAL_PORT, R_SENSE);                     // Hardware Serial
-//TMC2208Stepper driver(SW_RX, SW_TX, R_SENSE);                     // Software serial
 TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
-//TMC2209Stepper driver(SW_RX, SW_TX, R_SENSE, DRIVER_ADDRESS);
+
+#define FULL_STEPS_PER_REV 200 * 16 ///200 stepper motor steps * 16:1 gear reduction
 
 void setup() {
   pinMode(EN_PIN, OUTPUT);
@@ -35,32 +28,42 @@ void setup() {
 
   SERIAL_PORT.begin(115200);      // HW UART drivers
 
-  driver.begin();                 //  SPI: Init CS pins and possible SW SPI pins
-                                  // UART: Init SW UART (if selected) with default 115200 baudrate
+  driver.begin();
+                                  
   driver.toff(5);                 // Enables driver in software
-  driver.rms_current(600);        // Set motor RMS current
+  driver.rms_current(800);        // Set motor RMS current
       
 
-  //driver.en_spreadCycle(false);   // Toggle spreadCycle on TMC2208/2209/2224
+  driver.en_spreadCycle(true);   // Toggle spreadCycle on TMC2208/2209/2224
   driver.pwm_autoscale(true);     // Needed for stealthChop
+
+  
+  
 }
 
 bool shaft = false;
-int microsteps[] = {16, 32, 64, 128, 256};
+int microsteps[] = {1, 16, 32, 64, 128, 256};
 void loop() {
-  for (size_t i = 0; i < 5; i++)
+  for (size_t ustep = 0; ustep < 6; ustep++)
   {
-    driver.microsteps(microsteps[i]);    
-    //Serial.print("Sel ustep val ");
-    //Serial.println(i);
-    // Run 5000 steps and switch direction in software
-    for (uint16_t i = 10000; i>0; i--) {
+    driver.microsteps(microsteps[ustep]);    
+    
+
+    int step_period_us = 80;
+
+    for (int i = FULL_STEPS_PER_REV * microsteps[ustep]; i>0; i--) {
       digitalWrite(STEP_PIN, HIGH);
-      delayMicroseconds(160);
+      delayMicroseconds(step_period_us);
       digitalWrite(STEP_PIN, LOW);
-      delayMicroseconds(160);
+      delayMicroseconds(step_period_us);
 
       yield(); //avoid the WDT
+      if (i % 1000 == 0)
+      {
+        Serial.printf("uSteps: %d ", microsteps[ustep]);
+        Serial.printf("i = %d out of %d\n", i, FULL_STEPS_PER_REV * microsteps[ustep]);
+      }
+      
     }
     shaft = !shaft;
     //Serial.print("Shaft switched ");
@@ -68,7 +71,8 @@ void loop() {
     driver.shaft(shaft);
     //digitalWrite(DIR_PIN, shaft);
     digitalWrite(LED_BUILTIN, shaft);
-    }
+  }
+
   
-  
+
 }
